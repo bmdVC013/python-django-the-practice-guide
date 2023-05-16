63d47c266fc7e996beff86f991f82757e3f5ddd6
Dung Bui <bmdVC013@gmail.com>
2023-05-16 23:49:03 +0700

    :sparkles: Adding more dynamic view logic

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+MONTH_CHALLENGE_MAPPER = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "november": "Eat no meat for the entire month!",
+    "october": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!",
+}
+
 # Create your views here.
 
 
@@ -9,10 +24,8 @@ def monthly_challenge_by_number(request, month):
 
 
 def monthly_challenge(request, month):
-    MONTH_CHALLENGE_MAPPER = {
-        "january": "Eat no meat for the entire month!",
-        "february": "Walk for at least 20 minutes every day!",
-        "march": "Learn Django for at least 20 minutes every day!",
-    }
-
-    return HttpResponseNotFound("This month is not supported!") if MONTH_CHALLENGE_MAPPER.get(month) == None else HttpResponse(MONTH_CHALLENGE_MAPPER[month])
+    try:
+        challenge_text = MONTH_CHALLENGE_MAPPER[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
