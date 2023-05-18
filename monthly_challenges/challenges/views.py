8a5c1cb6c61b14d3cf75bb10c2533c6f5c8d30e1
Dung Bui <bmdVC013@gmail.com>
2023-05-19 00:10:44 +0700

    :sparkles: Setup tags in DTL

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -14,24 +14,18 @@ MONTH_CHALLENGE_MAPPER = {
     "september": "Learn Django for at least 20 minutes every day!",
     "november": "Eat no meat for the entire month!",
     "october": "Walk for at least 20 minutes every day!",
-    "december": "Learn Django for at least 20 minutes every day!",
+    "december": None,
 }
 
 # Create your views here.
 
 
 def index(request):
-    list_items = ""
     months = list(MONTH_CHALLENGE_MAPPER.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 def monthly_challenge_by_number(request, month):
     months = list(MONTH_CHALLENGE_MAPPER.keys())
