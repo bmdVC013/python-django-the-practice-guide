aafbc261fad7bcc9355d56458161c12709fdc037
Dung Bui <bmdVC013@gmail.com>
2023-08-17 00:32:20 +0700

    :sparkles: Build data and model for the blog page

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
+    path("admin/", admin.site.urls),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
 ]
