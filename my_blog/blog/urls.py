c518460b8c026102a0804b10bc36ad20b12eac13
Dung Bui <bmdVC013@gmail.com>
2023-09-10 23:24:51 +0700

    :sparkles: Building a Blog - Forms, Files & Sessions

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
+    path("", views.StartingPageView.as_view(), name="starting-page"),
     path("admin/", admin.site.urls),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+    path("posts", views.AllPostView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.DetailPostView.as_view(), name="post-detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
